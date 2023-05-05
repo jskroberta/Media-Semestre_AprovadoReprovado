6bc7f5d3b428cd57b9d3c54ac173184cf250ab9f
jskroberta <129328635+jskroberta@users.noreply.github.com>
2023-05-05 15:03:28 -0300

    3º atualização

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ function mediaSem() {
     let notasSem2 = document.getElementById("2Sem").value;
     let notasSem3 = document.getElementById("3Sem").value;
     
-    let notasTotal = (notasSem1 + notasSem2 + notasSem3) / 3;
-    console.log(notasTotal);
-    if (notasTotal => 6) {
-        document.getElementById("saidaResult").innerText= "Aprovado";
-    } else{
-        document.getElementById("saidaResult").innerText= "Reprovado";
-    }   
+    let notas1numero = parseFloat(notasSem1);
+    let notas2numero = parseFloat(notasSem2);
+    let notas3numero = parseFloat(notasSem3);
+
+    const resultFinal = (notas1numero + notas2numero + notas3numero) / 3;
+
+    //let notasTotal = parseFloat((notasSem1 + notasSem2 + notasSem3)) / 3;
+    console.log("b"+ resultFinal);
+    //if (notasTotal >= 6) {
+    //    document.getElementById("saidaResult").innerText= "Aprovado";
+    //} else{
+    //    document.getElementById("saidaResult").innerText= "Reprovado";
+    //}   
     
     //window.location.href ="segundapagina.html"
 }
