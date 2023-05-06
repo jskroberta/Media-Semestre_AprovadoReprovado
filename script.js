811b94d326e8dc4c63adfe8f8d7088f58e707837
jskroberta <129328635+jskroberta@users.noreply.github.com>
2023-05-06 19:58:17 -0300

    4º atalização
    Atualização do CSS, JS funcionando

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ function mediaSem() {
 
     const resultFinal = (notas1numero + notas2numero + notas3numero) / 3;
 
-    //let notasTotal = parseFloat((notasSem1 + notasSem2 + notasSem3)) / 3;
-    console.log("b"+ resultFinal);
-    //if (notasTotal >= 6) {
-    //    document.getElementById("saidaResult").innerText= "Aprovado";
-    //} else{
-    //    document.getElementById("saidaResult").innerText= "Reprovado";
-    //}   
+
+    if (resultFinal>= 6) {
+        document.getElementById("saidaResult").innerText= "Aprovado";
+    } else{
+        document.getElementById("saidaResult").innerText= "Reprovado";
+    }   
     
+   
     //window.location.href ="segundapagina.html"
+     //let notasTotal = parseFloat((notasSem1 + notasSem2 + notasSem3)) / 3;
 }
